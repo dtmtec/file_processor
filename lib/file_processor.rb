170a1ac35332d52cd764589f30c5e5d703e99ab1
Vicente Mundim <vicente.mundim@gmail.com>
2013-05-29 19:30:49 -0300

    Removing API design comments

--- lib/file_processor.rb
+++ lib/file_processor.rb
@@ -10,27 +10,3 @@ end
 
 require "file_processor/temp_file"
 require "file_processor/csv"
-
-### API 1
-# FileProcessor::CSV.open(filename, options) do |csv|
-#   csv.count # 527
-#   csv.encoding # utf-8
-#   csv.sample_data(4) # returns the first 4 rows, as an array of arrays
-#   csv.col_sep
-#   csv.headers
-#
-#   csv.each_with_index do |row|
-#     # process
-#   end
-#
-# end # close csv
-#
-# ### API 2
-# csv = FileProcessor::CSV.new(filename, options)
-# csv.count # 527
-#
-# csv.each_with_index do |row, index|
-#   # process
-# end
-# csv.close
-#
